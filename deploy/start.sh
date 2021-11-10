30c6d01beaf31e3afa4c2ab2a4a322040dc850e5
mrgeneralgoo <mrgeneralgoo@gmail.com>
2021-11-10 11:47:14 +0800

    Fix docker env invalid

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,9 +2,8 @@
 
 # process by githun actions
 # composer install -o -d /www --no-dev
-
 # php /www/artisan vendor:publish --all
-# php /www/artisan config:clear
-# php /www/artisan config:cache
+php /www/artisan config:clear
+php /www/artisan config:cache
 
 while : ;do php /www/artisan schedule:run; sleep 59; done;
